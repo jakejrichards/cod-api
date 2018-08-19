33789dc9c749024d0e501f1392b2c28f9ab60712
jakejrichards <jjrichar@go.olemiss.edu>
2018-08-18 22:48:17 -0500

    Refactor: Move to promise style api calls and use axios over request library

--- example/example.js
+++ example/example.js
@@ -1,29 +1,49 @@
-const { getProfile, getRecentMatches, getRecentSummary, getLeaderboards } = require('../index'); // require('cod-api');
+const { getLeaderboards, getProfile, getRecentMatches, getRecentSummary } = require('../index'); // require('cod-api');
 
-var options = {
-    title: "wwii", // bo3, iw, wwii
-    platform: "psn", // psn, xbl, steam
-    username: "Consisttt", // username
-    days: 1, // amount of days (required for recent matches & summary),
-    type: "core", // core, hc, arena
-    time: "monthly", // alltime, monthly, weekly
-    mode: "war" // career, war (Team Deathmatch), dm (Free-For-All), conf (Kill Confirmed), ctf (Capture The Flag), sd (Search & Destroy), dom (Domination), ball (Gridiron), hp (Hardpoint), 1v1, raid (War)
-};
+const title = "wwii"; // bo3, iw, wwii
+const platform = "psn"; // psn, xbl, steam
+const username = "Consisttt"; // username
+const days = 7; // amount of days (required for recent matches & summary),
+const type = "core"; // core, hc, arena
+const time = "monthly"; // alltime, monthly, weekly
+const mode = "war"; // career, war (Team Deathmatch), dm (Free-For-All), conf (Kill Confirmed), ctf (Capture The Flag), sd (Search & Destroy), dom (Domination), ball (Gridiron), hp (Hardpoint), 1v1, raid (War)
 
-getProfile(options, profile => {
-    console.log(profile);
-});
+getProfile({ title, platform, username })
+    .then(profile => {
+        // Do something with the profile object
+        console.log(profile);
+    })
+    .catch(err => {
+        // Do something with this error
+        console.log(err);
+    });
 
-getRecentMatches(options, matches => {
-    console.log(matches);
-});
+getRecentMatches({ title, platform, username, days })
+    .then(profile => {
+        // Do something with the recent matches object
+        console.log(profile);
+    })
+    .catch(err => {
+        // Do something with this error
+        console.log(err);
+    });
 
-getRecentSummary(options, summary => {
-    console.log(summary);
-});
+getRecentSummary({ title, platform, username, days })
+    .then(profile => {
+        // Do something with the recent summary object
+        console.log(profile);
+    })
+    .catch(err => {
+        // Do something with this error
+        console.log(err);
+    });
 
-getLeaderboards(options, leaderboards => {
-    console.log(leaderboards);
-});
-
-
+getLeaderboards({ title, platform, time, type, mode, username })
+    .then(profile => {
+        // Do something with the leaderboards object
+        console.log(profile);
+    })
+    .catch(err => {
+        // Do something with this error
+        console.log(err);
+    });
